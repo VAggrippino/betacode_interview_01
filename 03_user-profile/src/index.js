0d4a2b5ca383f8f4e53a561448953f20997adb19
Vince Aggrippino <vinceaggrippino@gmail.com>
2019-01-03 10:22:06 +0800

    Add documentation comment

--- 03_user-profile/src/index.js
+++ 03_user-profile/src/index.js
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * 
+ * 3. Write a react form to update user profile.
+ *     - Fields: first name, last name, email, birth date, preferred job title,
+ *       number of years experience
+ *     - send data to dummy end point https://api.dummyendpoint/me/profile with
+ *       http PUT
+ *     - validate fields name and email to be required
+ * 
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
